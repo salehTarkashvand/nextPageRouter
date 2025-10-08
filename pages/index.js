b02c1393776edb985db8bba3ab8cfa9c9af3f34a
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-08 16:31:10 +0330

    Navigating To Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Link from "next/link";
 export default function HomePage() {
+  const clients = [
+    { id: "saleh", name: "torkashvand" },
+    { id: "hamed", name: "torkashvand" },
+  ];
+
   return (
     <div>
       <h1>Home Page</h1>
       <ul>
-        <li>
-          <Link href="/portfolio">portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">clients</Link>
-        </li>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );

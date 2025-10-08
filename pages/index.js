e8d18f22a4b4b8492cd20dacb829913e532c6a96
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-08 16:06:52 +0330

    Navigating with the Link Component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }

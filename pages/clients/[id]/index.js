c086013de1286e24a1c8ba6709727c430dbe71eb
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-08 17:24:19 +0330

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,14 +1,19 @@
 import { useRouter } from "next/router";
 
 export default function clientProjectsPage() {
-    const router = useRouter()
-    console.log(router.query);
-    
-    return (
-        <div>
-            <h1>
-                the projects of a given client
-            </h1>
-        </div>
-    );
+  const router = useRouter();
+  console.log(router.query);
+  function projectHandler() {
+    // Navigating Programmatically
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "saleh", clientProjectId: "torkashvand" },
+    });
+  }
+  return (
+    <div>
+      <h1>the projects of a given client</h1>
+      <button onClick={projectHandler}>load project one</button>
+    </div>
+  );
 }

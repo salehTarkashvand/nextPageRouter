3bbfc27598ed31033c4bee66e09058b9a63f2c15
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-08 17:30:22 +0330

    adding a Custom 404 Page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+export default function notFound() {
+    // adding a Custom 404 Page
+    return (
+        <div>
+            <h1>
+                not found page
+            </h1>
+        </div>
+    );
+}
